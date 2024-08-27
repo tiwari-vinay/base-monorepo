84bc0064d28f4ea54bd211ea8540fbae23eab314
tiwari-vinay <itcgel@gmail.com>
2024-08-28 02:54:36 +0530

    feat: complete the express-js basic authentication apis

---
+++ apps/express-js-auth-apis/src/app.js
@@ -0,0 +1,82 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+import { userModel } from './model/user.js';
+
+const URL =
+  'mongodb+srv://itcgel:HO2SvVZMossUdMe6@cluster0.mximc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(URL);
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/users', async (req, res) => {
+  const users = await userModel.find();
+  res.status(200).json({ message: 'ok', users: [users] });
+});
+app.post('/auth/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await userModel.findOne({ username });
+
+  if (!user) {
+    return res.status(400).json({ message: 'User does not exist' });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (isMatch) {
+    const token = jwt.sign(
+      { userId: user._id, username: user.username },
+      process.env.SECRET_KEY || 'this is secret to sign with'
+    );
+    return res.status(200).json({ message: 'ok', access_token: token });
+  }
+
+  return res.status(400).json({ message: 'invalid user/passwor ' });
+});
+
+app.post('/auth/register', async (req, res) => {
+  const { email, password, username } = req.body;
+
+  if (
+    !email ||
+    typeof email != 'string' ||
+    !password ||
+    typeof password != 'string' ||
+    !username
+  )
+    return res.status(400).json({ message: 'Invalid payload' });
+
+  const encryptedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    const response = await userModel.create({
+      email,
+      username,
+      password: encryptedPassword,
+    });
+    return res
+      .status(201)
+      .json({ message: { username: response.username, id: response._id } });
+  } catch (err) {
+    if (err.code === 11000)
+      return res
+        .status(400)
+        .json({ message: 'Email or username already exists' });
+    return res.status(500).json({ message: 'Failed to create user' });
+  }
+});
+
+let port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
